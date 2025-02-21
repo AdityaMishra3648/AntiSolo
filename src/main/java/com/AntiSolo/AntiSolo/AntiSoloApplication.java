5d5b704e2e50bdb5de9761ba433ec8d2a6999834
adityamishra3648 <mk6748134@gmail.com>
2025-02-21 15:47:54 +0530

    initial commit

---
+++ src/main/java/com/AntiSolo/AntiSolo/AntiSoloApplication.java
@@ -0,0 +1,13 @@
+package com.AntiSolo.AntiSolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiSoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiSoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AntiSolo/AntiSolo/AntiSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AntiSolo.AntiSolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiSoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
