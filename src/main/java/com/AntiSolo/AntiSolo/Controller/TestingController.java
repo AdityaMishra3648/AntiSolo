fabaafd7a9003d7a28d16e01b26a967164194972
Aditya Mishra <123090809+AdityaMishra3648@users.noreply.github.com>
2026-03-15 14:25:39 +0530

    Update greeting message in TestingController

--- src/main/java/com/AntiSolo/AntiSolo/Controller/TestingController.java
+++ src/main/java/com/AntiSolo/AntiSolo/Controller/TestingController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestingController {
     @GetMapping("/hello")
     public String greet(){
-        return "Hello world!";
+        return "Hello world!!!!!!!!!!";
     }
 }
